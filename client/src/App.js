d8b19f40d00211eb28b5a366ea30090468612360
Basavachari <basavachariboppudi@gmail.com>
2023-07-25 13:42:19 +0530

    Tried to fix error

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
