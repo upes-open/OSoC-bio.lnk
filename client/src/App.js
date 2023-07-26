dbaa092a6162ccb9381db0b55342e1526dba049c
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2023-07-26 16:54:47 +0530

    Implemented Custom Login Using Email and Password Under issue #21
    Used Local Strategy on Passport.js to add backend for creating simple register, login and logout functionality.
    
    To check the endpoints send Post requests to the specified api end points ("/register", "/login", "check-session") using terminal or postman with the required JSON payload.

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+// import Login from './Components/Login/Login';
+// import Signup from './Components/Login/Signup';
+// import Dashboard from './Components/Login/Dashboard';
+import Footer from './Components/Footer/Footer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} /> */}
+          {/* <Route index element={<Login />} /> */}
+          {/* Add other routes or components as needed */}
+          {/* <Route path="/profile" element={<UserProfile />} /> */}
+          {/* <Route path="/settings" element={<UserSettings />} /> */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
